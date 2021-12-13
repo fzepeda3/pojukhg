4ac48cb7accd6559428e0074625ddf8a7c857d55
fzepeda3 <96065034+fzepeda3@users.noreply.github.com>
2021-12-13 09:38:10 -0600

    stooof

--- images.g.ts
+++ images.g.ts
@@ -202,6 +202,25 @@ f 2 2 2 f f f . . . . . f f f 1
 f f f f f . . . . . . . . f f f 
 f . . . . . . . . . . . . . . . 
 `;
+            case "image4":
+            case "cpuScissors":return img`
+. . . . . . . . . . . . . . . f 
+f f f . . . . . . . . f f f f f 
+1 f f f . . . . . f f f 2 2 2 f 
+1 1 1 f f f . f f f 2 2 2 2 2 f 
+f 1 1 1 1 f f 2 2 2 2 2 2 2 2 f 
+f f 1 1 1 1 f 2 2 2 2 2 2 f f f 
+f f f f f 1 1 1 2 2 2 f f . . . 
+f f f f f 1 1 1 2 f f f . . . . 
+f f 1 1 1 1 1 2 2 2 2 f f f . . 
+f 1 1 1 1 f f 2 2 2 2 2 2 f f f 
+1 1 1 1 f . . f f f 2 2 2 2 2 f 
+1 1 f f f . . . . f f f 2 2 2 f 
+f f f . . . . . . . . f f 2 2 f 
+. . . . . . . . . . . . f f f f 
+. . . . . . . . . . . . . . f f 
+. . . . . . . . . . . . . . . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-let mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-controller.moveSprite(mySprite)
+function Input () {
+    if (userInput == 1) {
+        userAction = "Rock"
+    } else if (userInput == 2) {
+        userAction = "Scissors"
+    } else if (userInput == 3) {
+        userAction = "Paper"
+    } else {
+        game.splash("invalid input")
+        break;
+    }
+}
+let userAction = ""
+let userInput = 0
 scene.setBackgroundImage(assets.image`BK1`)
+let userWinner = 0
+while (userWinner < 2) {
+    userInput = game.askForNumber("", 1)
+    if (true) {
+    	
+    }
+    Input()
+}
